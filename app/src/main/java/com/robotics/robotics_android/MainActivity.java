d4ee3a601b8bc50821222b9b345822b4c04f866f
EduhG <edwinnyangena@gmail.com>
2016-09-22 15:36:18 +0300

    Updated activity_main.xml

--- app/src/main/java/com/robotics/robotics_android/MainActivity.java
+++ app/src/main/java/com/robotics/robotics_android/MainActivity.java
@@ -2,12 +2,29 @@ package com.robotics.robotics_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button forward, back, turnLeft, turnRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        forward = (Button) findViewById(R.id.forward);
+        back = (Button) findViewById(R.id.back);
+        turnLeft = (Button) findViewById(R.id.turnLeft);
+        turnRight = (Button) findViewById(R.id.turnRight);
+
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(this, "Moving Forward", );
+            }
+        });
     }
 }
